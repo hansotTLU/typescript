de53d4ac174d3f6e29529cfb111db736fcba2e91
Hans Oskar Trolla <146342767+hansotTLU@users.noreply.github.com>
2024-09-20 20:04:03 +0300

    fix: gh pages deploy
    replaced "createBrowserRouter" with "createHashRouter" in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,54 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import reportWebVitals from "./reportWebVitals"
 
 import {
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import App from "./App";
-import Login from "./components/Login";
-import HomePage from "./components/HomePage";
-import Me from "./components/Me";
+} from "react-router-dom"
+import App from "./App"
+import Login from "./components/Login"
+import HomePage from "./components/HomePage"
+import Me from "./components/Me"
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/me" element={<Me />} />
-      <Route path="/contact" element={<h1>Contact</h1>} />
-    </Route>
-  )
-);
+    <Route
+      path="/"
+      element={<App />}
+    >
+      <Route
+        index
+        element={<HomePage />}
+      />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+      <Route
+        path="/me"
+        element={<Me />}
+      />
+      <Route
+        path="/contact"
+        element={<h1>Contact</h1>}
+      />
+    </Route>,
+  ),
+)
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
